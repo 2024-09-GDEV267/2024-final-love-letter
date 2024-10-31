31b827d1bc037070fe789f5e3f845d76e70254b3
David T <30881978+DavidT1826@users.noreply.github.com>
2024-10-31 12:33:15 -0400

    Establish Value?

--- Assets/Scenes/Deck.cs
+++ Assets/Scenes/Deck.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
+    private Card[] cards;
     
     void Start()
     {
-        
+        for 
     }
 
     // Update is called once per frame
